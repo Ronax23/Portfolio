1e2a1f9e4b50b8152c2d5df7233fad71f427cea4
Bhavya <48246206+Ronax23@users.noreply.github.com>
2025-09-08 22:23:24 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -158,6 +158,30 @@ let Tex=formControl.querySelector("#Texm").value;
 
 
 
+  $('.owl-carousel').owlCarousel({
+ autoplay: true,        
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+
+
+    loop:true,
+    margin:40,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
+
+
 
 
 
